72718d6d895280aefb20f714c07d1c8810950e81
morrss <grishkovea@gmail.com>
2024-10-03 17:34:43 +0300

    исправил баг со счетчиком

--- src/main/java/springApp/models/Person.java
+++ src/main/java/springApp/models/Person.java
@@ -2,11 +2,13 @@ package springApp.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Person {
     private static int PEOPLE_COUNTER = 0;
 
