6781a5197f51acefcef85848521aa96470e95300
morrss <grishkovea@gmail.com>
2024-10-03 16:36:11 +0300

    реализовал эндпоинт для отображения конкретного человека

--- src/main/java/springApp/controllers/PeopleController.java
+++ src/main/java/springApp/controllers/PeopleController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import springApp.DAO.PeopleDAO;
 
@@ -20,6 +21,11 @@ public class PeopleController {
         return "people";
     }
 
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", peopleDAO.read(id));
+        return "/show";
+    }
 
 
 }
