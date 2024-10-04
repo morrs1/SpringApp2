cafff6440d0db60f470b607f6517abd4c4e340ef
morrs <grishkovea@gmail.com>
2024-10-04 21:46:43 +0300

    добавил валидацию данных

--- src/main/java/springApp/models/Person.java
+++ src/main/java/springApp/models/Person.java
@@ -1,5 +1,9 @@
 package springApp.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,8 +16,15 @@ import lombok.Setter;
 public class Person {
 
     private int id;
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     private String name;
+    @NotEmpty
+    @Size(min = 2, max = 30, message = "Surname should be between 2 and 30 characters")
     private String surname;
+    @Min(value = 0, message = "Age should be greater then 0")
     private int age;
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
     private String email;
 }
